83584437e5f078dac2bcbb1bd32ea7826acb2e12
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-04-20 16:11:31 -0300

    Estudando

--- aula-1/src/components/RenderIList.js
+++ aula-1/src/components/RenderIList.js
@@ -1,15 +1,19 @@
 function RenderList(){
 
-  const itens = ['João', 'Maria', 'Chucriti', 'Will', 'Flash', 'Chaveco']
+  const itens = ['João', 'Maria', 'Chucruiti', 'Will', 'Flash', 'Chaveco']
                 //Impossível ser mais aleatório do que isso. kkkkk
 
   return(
     <div className="box">
       <h2>Renderizando Listas</h2>
       {
-        itens.map(item => (
-          <p>{item}</p>
-        ))
+        itens.length > 0 ? (
+          itens.map((item, index) => (
+            <p key={index}> {item} </p>
+          ))
+        ) : (
+          <p>Não há itens na lista!</p>
+        )
       }
     </div>
   )
