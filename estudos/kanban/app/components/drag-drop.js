f82f7c9d4f2f14e245d04b86a0ada6f9bdc80e8b
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-09-22 02:44:12 -0300

    Update drag-drop.js

--- estudos/kanban/app/components/drag-drop.js
+++ estudos/kanban/app/components/drag-drop.js
@@ -1,5 +1,5 @@
 
-let currentList, draggingCard,
+let currentList, currentListId, draggingCard,
    draggingCardHeight, tempPosition, currentCard, 
    dragableArea, shadowCard;
 const dragDrop = {
@@ -8,24 +8,29 @@ const dragDrop = {
    dragStart: function(e){
       draggingCard = e.target.closest('.card');
       draggingCardHeight = e.target.offsetHeight
-
+      
       //Seleciona a lista sobre qual o draging está.
       currentList = e.target.closest('.list');
-      dragableArea = document.querySelector(`#${currentList.getAttribute('id')} .dragableArea`);
-      dragableArea.style.height = `${dragableArea.offsetHeight}px`
-
-     
+      currentListId = currentList.getAttribute('id');
+      dragableArea = document.querySelector(`#${currentListId} .dragableArea`);  
+      dragableArea.style.height = `${dragableArea.offsetHeight}px`;    
       
-      e.dataTransfer.setDragImage(draggingCard, 0, 0);
-      setTimeout(() => {
-         draggingCard.style.opacity='0'
-      }, 0);
-      setTimeout(() => {
-         draggingCard.style.display='none'
-         if(dragableArea.childNodes.length <= 1){
-            dragableArea.appendChild(TemporarieCard(draggingCardHeight));
-         }
-      }, 10);
+      // //Seta a imagem que aparecerá no dragging e a posição do mouse em no card arrastado.
+      // e.dataTransfer.setDragImage(draggingCard, 0, 0);
+
+      // setTimeout(() => {
+      //    draggingCard.style.opacity='0';
+      //    // dragableArea.appendChild(TemporarieCard(draggingCardHeight));
+      //    // dragableArea.r
+      // }, 0);
+      // setTimeout(() => {
+      //    //Insere um shadowCard na dragableArea se não haver nehum card 
+      //    //depois do card ser arrastado.
+      //    draggingCard.style.display='none';
+      //    if(dragableArea.childNodes.length <= 1){
+            
+      //    }
+      // }, 10);
    },
 
    dragEnter: function(e){
@@ -35,26 +40,29 @@ const dragDrop = {
          shadowCard = document.querySelector('.shadow-card');
          shadowCard ? shadowCard.remove() : '';
          tempPosition = '';
-         dragableArea.style.height='auto'
+         dragableArea.style.height='auto';
       }
       //Atualiza a lista sobre qual o draging está.
       currentList = e.target.closest('.list');
+      currentListId = currentList.getAttribute('id');
       dragableArea = document.querySelector(`#${currentList.getAttribute('id')} .dragableArea`);
    },
    
    dragOver: function(e){
       e.preventDefault(); //obrigatório.
-      
+      shadowCard = TemporarieCard(draggingCardHeight)
+      const card = e.target.closest('.card');
       //Se a dragableArea não conter nenhum card, adicioana a divTemp
       //para depois poder ser substituida pelo card dropado.
       if(dragableArea.childNodes.length < 1){
-         dragableArea.appendChild(TemporarieCard(draggingCardHeight));
+         dragableArea.appendChild(shadowCard);
+         return
       }
       if(dragableArea.childNodes.length == 1 && dragableArea.firstChild == draggingCard){
-         dragableArea.appendChild(TemporarieCard(draggingCardHeight));
+         dragableArea.appendChild(shadowCard);
+         return
       }
 
-      const card = e.target.closest('.card');
       if(!card) return;
 
       currentCard = card;
@@ -63,23 +71,35 @@ const dragDrop = {
       const cardHalf = card.offsetHeight / 2; //Tamaho do card / 2 
 
       //Verifica se o card arrartado é o mesmo que o que está sobreposto.
-      if(draggingCard == e.target.closest('.card')) return;
+      if(draggingCard == card) return;
       
       if(mousePositionY < cardPositionY + cardHalf){
-         if(tempPosition == 'up') return;
-         //remove a 'div temporária' criada anteriomente.
-         tempPosition == 'down' ? document.querySelector('.shadow-card').remove() : '';
-            
-         //Insere a caixa temporaria acima do card
-         card.insertAdjacentElement('beforebegin', TemporarieCard(draggingCardHeight))
-         tempPosition = 'up';
-      }else{
-         if(tempPosition == 'down') return;
+
+         if(!card.previousSibling){
+            return card.insertAdjacentElement('beforebegin', shadowCard)
+         }
+         if(card.previousSibling.isEqualNode(shadowCard)) return;
          
-         tempPosition == 'up'? document.querySelector('.shadow-card').remove() : '';
+         if(card.nextSibling){
+            console.log('aru')
+            card.nextSibling.isEqualNode(shadowCard) ? shadowCard.remove() : '';
+         }
+         
+         //Insere a caixa temporaria acima do card
+         card.insertAdjacentElement('beforebegin', shadowCard)
+      }else{
+         if(!card.nextSibling){
+            return card.insertAdjacentElement('afterend', shadowCard);
+         }
+         if(card.nextSibling.isEqualNode(shadowCard)) return;
+
+         if(card.previousSibling){
+            card.previousSibling.isEqualNode(shadowCard) ? shadowCard.remove() : '';
+            // console.log('tem')
+         }
+
          //Insere a caixa temporaria abaixo do card
-         card.insertAdjacentElement('afterend', TemporarieCard(draggingCardHeight))
-         tempPosition = 'down';
+         card.insertAdjacentElement('afterend', shadowCard)
       }
    },
 
@@ -88,7 +108,7 @@ const dragDrop = {
       draggingCard.style.opacity='1'
       //Tenta selecionar a shadowCard, se não conseguir, é porque não tem como dropar o card, então retorna 
       //e reseta as variáveis.
-      shadowCard = (document.querySelector(`#${currentList.getAttribute('id')} .shadow-card`));
+      shadowCard = (document.querySelector(`#${currentListId} .shadow-card`));
       if(!shadowCard){
          dragDrop.reseteVariables(); 
          return;
@@ -102,7 +122,7 @@ const dragDrop = {
 
    reseteVariables: function(){
       currentList = draggingCard = draggingCardHeight = tempPosition = currentCard 
-      = dragableArea, shadowCard = null;
+      = dragableArea, shadowCard = currentListId = null;
    }
 }
 
