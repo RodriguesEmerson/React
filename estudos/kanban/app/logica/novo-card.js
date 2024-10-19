453f5b9a687eed433a72e5b5e98d25983f9f41cc
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-10-19 18:59:59 -0300

    Update novo-card.js

--- estudos/kanban/app/logica/novo-card.js
+++ estudos/kanban/app/logica/novo-card.js
@@ -1,22 +1,27 @@
 export class NovoCard {
-   constructor(content, UUID, dataHoje){
+   constructor(content, UUID, dataHoje) {
       this.content = content;
       this.data = dataHoje;
       this.prazo = false;
+      this.periodo = {
+         inicio: "",
+         fim: "",
+         status: false
+      }
       this.img = '';
       this.capa = {
          color: false, full: false,
          img: ""
       },
-      this.integrants = [];
+         this.integrants = [];
       this.labels = [];
       this.coments = [];
       this.id = `c${UUID}`;
-   }   
+   }
 }
 
 const criarNovo = {
-   card: function(e, cards, setCards){
+   card: function (e, cards, setCards) {
       e.preventDefault();
       const parentId = e.target.getAttribute('data-parent');
       const form = document.querySelector(`#f${parentId}`);
@@ -24,9 +29,9 @@ const criarNovo = {
       //Pega os dados do formilário
       const formData = new FormData(form);
       const texto = Object.fromEntries(formData)['texto'];
-      
+
       //Valdia o texto do card.
-      if(texto.length == 0) return console.log('Nome Inválido!');
+      if (texto.length == 0) return console.log('Nome Inválido!');
 
       //Instancia um novo card.
       const novoCard = new NovoCard(texto, this.gerarUUID(), this.dataHoje());
@@ -37,18 +42,18 @@ const criarNovo = {
       this.salvarCard(novoCard, parentId);
 
    },
-   gerarUUID: function() {
+   gerarUUID: function () {
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
          const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
          return v.toString(16);
       })
    },
-   dataHoje: function(){
-      return new Date().toLocaleDateString('pt-br', {year: 'numeric', month: '2-digit', day: '2-digit'});
+   dataHoje: function () {
+      return new Date().toLocaleDateString('pt-br', { year: 'numeric', month: '2-digit', day: '2-digit' });
    },
 
-   salvarCard: async function(novoCard, parentId){
-      await fetch (`/api/projects/${parentId}`, 
+   salvarCard: async function (novoCard, parentId) {
+      await fetch(`/api/projects/${parentId}`,
          {
             method: 'POST',
             headers: {
@@ -59,7 +64,7 @@ const criarNovo = {
          .then(respose => respose.status == 201 && this.notificar('novoCard'))
          .catch(error => console.log(error));
    },
-   notificar: function(tipo){
+   notificar: function (tipo) {
       tipo == 'novoCard' && console.log('Novo card criado com sucesso!');
    }
 }
